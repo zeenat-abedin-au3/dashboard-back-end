10f300e03ccbdd6c81777c37f08d6bca1080c793
zeenat-abedin-au3 <zeenu.23@gmail.com>
2020-04-12 14:06:27 +0530

    feat: error, async middlewares

---
+++ middlewars/erros.js
@@ -0,0 +1,25 @@
+const ErrorResponse = require("../utils/errorResponse");
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
+  //   Mongoose bad ObjectId
+  if (err.name === "CastError") {
+    const message = `Resources not found with the id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  //   Mongoose Validation Error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Server Error",
+  });
+};
+
+module.exports = errorHandler;
